bb79f2179bb25e7bec0606412beab7fe71048f70
medchakhman <medchakhman@gmail.com>
2022-09-19 17:27:33 +0000

    add changes nginx

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=mazal159630
-export POSTGRES_HOST=microservice-db.chxeugzkr0sv.us-east-2.rds.amazonaws.com
+export POSTGRES_HOST=database-1.cdlieqo3tsoh.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::es-monolith-to-ms
+export AWS_BUCKET=arn:aws:s3:::monolith-to-mc
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
